c1a65adafba4f44d0a136ddbed510bda9989fc0a
Mabel Christina <maybelchristina@gmail.com>
2021-11-29 15:23:28 +0530

    [Mabel] Add: CAl class

---
+++ DemoProject/DemoProject/Cal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoProject
+{
+    class Cal
+    {
+        public void Add()
+        {
+            int a, b, c;
+            c = a + b;
+
+        }
+    }
+}
